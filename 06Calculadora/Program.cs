c34bf3276bebb8fff53635c6c65c6ce5010dacaa
izabela <izabela.leme@plasutil.com.br>
2023-02-14 08:35:59 -0300

    Adicionando um projeto calculadora

---
+++ 06Calculadora/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Numa situação onde precisamos de uma calculadora, o jeito mais eficaz de trabalhar é com arrays
+            int result = Calculadora.Sum(new int[] { 10, 20, 30, 40 });
+
+
+        }
+    }
+}
