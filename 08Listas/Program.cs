16bc3bf194e0cb7cd1fd110fe4466fd14a2162fc
izabela <izabela.leme@plasutil.com.br>
2023-02-16 16:51:22 -0300

    Aula de Lista

---
+++ 08Listas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08Listas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> list = new List<string>();
+
+            list.Add("Venti");
+            list.Add("Kazuha");
+            list.Add("Gorou");
+            list.Add("Tighnari");
+
+            foreach (string obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+
+        }
+    }
+}
