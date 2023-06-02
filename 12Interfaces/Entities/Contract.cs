10ab4216bd85b2b9a7a075c1db70e99d08ab96af
Izabela Leme <izaleme00@gmail.com>
2023-06-02 17:11:58 -0300

    Interfaces project

---
+++ 12Interfaces/Entities/Contract.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Interfaces.Entities
+{
+    class Contract
+    {
+        public int Number { get; private set; }
+        public DateTime Date { get; private set; }
+        public double TotalValue { get; private set; }
+        public List<Installment> Installments { get; set; }
+
+        public Contract(int contractNumber, DateTime contractDate, double totalValue)
+        {
+            Number = contractNumber;
+            Date = contractDate;
+            TotalValue = totalValue;
+            Installments = new List<Installment>();
+        }
+
+        public void AddInstallment(Installment installment)
+        {
+            Installments.Add(installment);
+        }
+    }
+}
