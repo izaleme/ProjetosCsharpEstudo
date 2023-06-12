f0a4840c9c799945e2cee886209f79a424dbca72
Izabela Leme <izaleme00@gmail.com>
2023-06-12 16:09:44 -0300

    using HashSet

---
+++ 13Generics/Entities/HashSet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics.Entities
+{
+    class HashSet : HashSet<int>
+    {
+        HashSet<int> alumns = new HashSet<int>();
+
+        public void Alumns(string course)
+        {
+            Console.Write("\nHow many students for course " + course + "? ");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite as matrículas:");
+
+            for (int i = 0; i < num; i++)
+            {
+                alumns.Add(int.Parse(Console.ReadLine()));
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Total students: " + alumns.Count;
+        }
+    }
+}
---
+++ 13Generics/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Generics.Entities;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet hashSet= new HashSet();
+
+            hashSet.Alumns("A");
+            hashSet.Alumns("B");
+            hashSet.Alumns("C");
+
+            Console.WriteLine();
+            Console.WriteLine(hashSet.ToString());  // total de estudantes
+        }
+    }
+}
